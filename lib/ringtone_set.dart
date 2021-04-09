88be8f436ef528f745c8cc56add56391e9cdf377
Dmitry Belogub <dmitry.belogub@gmail.com>
2021-04-09 15:18:34 +0300

    Add options to set from file and from network

--- lib/ringtone_set.dart
+++ lib/ringtone_set.dart
@@ -1,11 +1,9 @@
 import 'dart:async';
 import 'dart:io';
 
-import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
+import 'package:http/http.dart' as http;
 import 'package:path_provider/path_provider.dart';
-import 'package:flutter/services.dart';
-
-import 'package:flutter/services.dart';
 
 class RingtoneSet {
   static const MethodChannel _channel = const MethodChannel('ringtone_set');
@@ -26,28 +24,71 @@ class RingtoneSet {
     return version;
   }
 
-  static Future<String> setRingtone(String asset) async {
+  static Future<String> setRingtoneFromAsset(String asset) async {
     final path =
         '${(await getTemporaryDirectory()).path}/${asset.split('/').last}';
     final file = File(path);
     final assetload = await rootBundle.load(asset);
     await file.writeAsBytes((assetload).buffer.asUint8List());
 
-    final String result =
-        await _channel.invokeMethod('setRingtone', {"path": path});
+    final String result = await setRingtoneFromFile(file);
 
     return result;
   }
 
-  static Future<String> setNotification(String asset) async {
+  static Future<String> setRingtoneFromNetwork(String url) async {
+    final path =
+        '${(await getTemporaryDirectory()).path}/${url.split('/').last}';
+    final file = File(path);
+    final response = await http.get(Uri.parse(url));
+    if (response.statusCode == 200) {
+      await file.writeAsBytes(response.bodyBytes);
+
+      final String result = await setRingtoneFromFile(file);
+
+      return result;
+    } else {
+      return "failure";
+    }
+  }
+
+  static Future<String> setRingtoneFromFile(File file) async {
+    final String result =
+        await _channel.invokeMethod('setRingtone', {"path": file.path});
+
+    return result;
+  }
+
+  static Future<String> setNotificationFromAsset(String asset) async {
     final path =
         '${(await getTemporaryDirectory()).path}/${asset.split('/').last}';
     final file = File(path);
     final assetload = await rootBundle.load(asset);
     await file.writeAsBytes((assetload).buffer.asUint8List());
 
+    final String result = await setNotificationFromFile(file);
+    return result;
+  }
+
+  static Future<String> setNotificationFromNetwork(String url) async {
+    final path =
+        '${(await getTemporaryDirectory()).path}/${url.split('/').last}';
+    final file = File(path);
+    final response = await http.get(Uri.parse(url));
+    if (response.statusCode == 200) {
+      await file.writeAsBytes(response.bodyBytes);
+
+      final String result = await setRingtoneFromFile(file);
+
+      return result;
+    } else {
+      return "failure";
+    }
+  }
+
+  static Future<String> setNotificationFromFile(File file) async {
     final String result =
-        await _channel.invokeMethod('setNotification', {"path": path});
+        await _channel.invokeMethod('setNotification', {"path": file.path});
     return result;
   }
 }
