9e367bd0d94025f02550f56b8fbfb5cc63215ce5
Dmitry Belogub <dmitry.belogub@gmail.com>
2021-09-09 16:18:19 +0300

    Add guess mime from file input stream
    Also changed the final fallback from audio/mp3 to audio/mpeg.

--- android/src/main/java/acr/rt/ringtone_set/RingtoneSetPlugin.java
+++ android/src/main/java/acr/rt/ringtone_set/RingtoneSetPlugin.java
@@ -17,6 +17,7 @@ import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 
 import android.Manifest;
@@ -87,24 +88,126 @@ public class RingtoneSetPlugin implements FlutterPlugin, MethodCallHandler {
         }
     }
 
-    private static String getMIMEType(String url, String downloadedMimeType) {
-        String mType = null;
+    private static String guessMimeTypeFromExtension(String path) {
+        String mimeType = null;
         String fileExtension = "";
         try {
-            int i = url.lastIndexOf('.');
+            int i = path.lastIndexOf('.');
             if (i > 0) {
-                fileExtension = url.substring(i + 1);
+                fileExtension = path.substring(i + 1);
             }
             if (fileExtension != "") {
-                mType = MimeTypeMap.getSingleton().getMimeTypeFromExtension(fileExtension);
+                mimeType = MimeTypeMap.getSingleton().getMimeTypeFromExtension(fileExtension);
             }
         } catch (Exception ignored) {
 
         }
-        if (mType == null) {
-            return java.util.Objects.requireNonNullElse(downloadedMimeType, "audio/mp3");
+
+        return mimeType;
+    }
+
+    static private String guessMimeTypeFromStream(InputStream is)
+            throws IOException {
+        // If we can't read ahead safely, just give up on guessing
+        if (!is.markSupported())
+            return null;
+
+        is.mark(16);
+        int c1 = is.read();
+        int c2 = is.read();
+        int c3 = is.read();
+        int c4 = is.read();
+        int c5 = is.read();
+        int c6 = is.read();
+        int c7 = is.read();
+        int c8 = is.read();
+        int c9 = is.read();
+        int c10 = is.read();
+        int c11 = is.read();
+        is.reset();
+
+        if (c1 == 0x2E && c2 == 0x73 && c3 == 0x6E && c4 == 0x64) {
+            return "audio/basic";  // .au format, big endian
         }
-        return mType;
+
+        if (c1 == 0x64 && c2 == 0x6E && c3 == 0x73 && c4 == 0x2E) {
+            return "audio/basic";  // .au format, little endian
+        }
+
+        if (c1 == 'R' && c2 == 'I' && c3 == 'F' && c4 == 'F') {
+            return "audio/x-wav";
+        }
+
+        if (c1 == 0x23 && c2 == 0x21 && c3 == 0x41 && c4 == 0x4D && c5 == 0x52) {
+            return "audio/amr";
+        }
+
+        if (c1 == 0x66 && c2 == 0x74 && c3 == 0x79 && c4 == 0x70 && c5 == 0x69 && c6 == 0x73
+                && c7 == 0x6F && c8 == 0x6D) {
+            return "audio/mp4";
+        }
+
+        if (c1 == 0x00 && c2 == 0x00 && c3 == 0x00 && c4 == 0x20 && c5 == 0x66 && c6 == 0x74
+                && c7 == 0x79 && c8 == 0x70 && c9 == 0x4D && c10 == 0x34 && c11 == 0x41) {
+            return "audio/mp4";
+        }
+
+        if (c1 == 0x4D && c2 == 54 && c3 == 68 && c4 == 64) {
+            return "audio/midi";
+        }
+
+        if (c1 == 0x1A && c2 == 0x45 && c3 == 0xDF && c4 == 0xA3) {
+            return "audio/x-matroska";
+        }
+
+        if (c1 == 0x30 && c2 == 0x26 && c3 == 0xB2 && c4 == 0x75 && c5 == 0x8E && c6 == 0x66
+                && c7 == 0xCF && c8 == 0x11) {
+            return "audio/x-ms-wma";
+        }
+
+        if (c1 == 0x4F && c2 == 0x67 && c3 == 0x67 && c4 == 0x53) {
+            return "audio/ogg";
+        }
+
+        if (c1 == 0x49 && c2 == 0x44 && c3 == 0x33) {
+            return "audio/mpeg";
+        }
+
+        if (c1 == 0xFF && (c2 == 0xFB || c2 == 0xF3 || c2 == 0xF2)) {
+            return "audio/mpeg";
+        }
+
+        return null;
+    }
+
+    private static String getMIMEType(String absolutePath, String downloadedMimeType) {
+        String mimeType = null;
+        mimeType = guessMimeTypeFromExtension(absolutePath);
+
+        if (mimeType != null) {
+            return mimeType;
+        }
+
+        try (InputStream inputStream = new BufferedInputStream(new FileInputStream(absolutePath))) {
+            mimeType = guessMimeTypeFromStream(inputStream);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        if (mimeType != null) {
+            return mimeType;
+        }
+
+        // downloadedMimeType obtained from "content-type" header
+        if (downloadedMimeType != null) {
+            // A little fix for a common server misconfiguration
+            if (downloadedMimeType == "audio/wav" ) {
+                return "audio/x-wav";
+            }
+            return downloadedMimeType;
+        }
+
+        return "audio/mpeg";
     }
 
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
@@ -118,7 +221,7 @@ public class RingtoneSetPlugin implements FlutterPlugin, MethodCallHandler {
                 ContentValues values = new ContentValues();
                 values.put(MediaStore.MediaColumns.DATA, mFile.getAbsolutePath());
                 values.put(MediaStore.MediaColumns.TITLE, "Custom ringtone");
-                values.put(MediaStore.MediaColumns.MIME_TYPE, getMIMEType(path, downloadedMimeType));
+                values.put(MediaStore.MediaColumns.MIME_TYPE, getMIMEType(mFile.getAbsolutePath(), downloadedMimeType));
                 values.put(MediaStore.MediaColumns.SIZE, mFile.length());
                 values.put(MediaStore.Audio.Media.ARTIST, "Ringtone app");
                 values.put(MediaStore.Audio.Media.IS_RINGTONE, isRingt);
